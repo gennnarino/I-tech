c040041b8ccd4462c29320747f512d0d8383379d
Stefano Foresta <forestastefano@gmail.com>
2017-01-08 19:49:29 +0100

    no message

---
+++ website/connessione.php
@@ -0,0 +1,19 @@
+
+    <?php 
+/*
+  creazione di un database con MySQLi.
+  La prima operazione richiesta sarà quella relativa alla definizione
+  del blocco dei parametri per la connessione
+*/
+// nome di host
+$host = "netflics.hopto.org";
+// username dell'utente in connessione
+$DBuser = "itech";
+// password dell'utente
+$DBpassword = "itech";
+
+$db ="negozio";
+// stringa di connessione al DBMS
+$connessione = new mysqli($host, $DBuser, $DBpassword, $db);
+
+?>
