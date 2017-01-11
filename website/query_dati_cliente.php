296b44ce88803d3ceee3e0fe9c696338f4fb8ddd
Stefano Foresta <forestastefano@gmail.com>
2017-01-11 16:02:36 +0100

    Fix page query_dati_cliente.php

--- website/query_dati_cliente.php
+++ website/query_dati_cliente.php
@@ -4,26 +4,21 @@
     $query = "SELECT * FROM cliente WHERE email = '$mail' AND passw = '$pass' ";
 
     $ris =mysqli_query($connessione,$query);
-    echo $query.'<br>';
-    echo $mail.'<br>';
-    echo $pass.'<br>';
     $quanti = mysqli_num_rows($ris);
-
     if ($quanti == 0)
     {
         echo "Nessun record!";
     }
     else
     {
-        for($x=0; $x<$quanti; $x++)
-        {
-            $rs = mysqli_fetch_row($query);
-            $cf = $rs[0];
-            $nome = $rs[1];
-            $cognome = $rs[2];
-            $indirizzo = $rs[3];
-            $telefono = $rs[6];
-        }
+        $rs = mysqli_fetch_assoc($ris);
+
+            $cf = $rs[cf];
+            $nome = $rs[nome];
+            $cognome = $rs[cognome];
+            $indirizzo = $rs[indirizzo];
+            $telefono = $rs[telefono];
+       
     }
     mysqli_close($connessione);
 ?>
