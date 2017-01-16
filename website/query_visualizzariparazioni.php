20bb53baf258d6e8174739fbdc495af98cdd06e8
Gennaro Franzese <gennnarino@gmail.com>
2017-01-16 13:15:30 +0100

    Inserimento preventivo e fix visualizza riparazione

--- website/query_visualizzariparazioni.php
+++ website/query_visualizzariparazioni.php
@@ -1,8 +1,27 @@
 <?php
 session_start(); 
-
+$user="a";
+$admin=false;
 include ("connessione.php");
-
+if($admin==false){
+$queryy = "SELECT cf FROM cliente WHERE email = '$user' ";
+	$ris= mysqli_query ($connessione,$queryy);
+	$rs = mysqli_fetch_assoc($ris);
+	$cf = $rs['cf'];
+$queryyy = mysqli_query($connessione,"SELECT * FROM riparazione where cliente = '$cf' ");
+    while($ciclo=mysqli_fetch_assoc($queryyy)){
+          
+            $id = $ciclo[idR];
+            $descrizione = $ciclo[descrizione];
+            $prezzo = $ciclo[prezzo];
+            $stato = $ciclo[stato];
+            $cliente = $ciclo[cliente];
+            include("script_tabella_riparazioni.php");
+    
+} 
+    
+} else if($admin==true){
+    
 $query = mysqli_query($connessione,"SELECT * FROM riparazione");
 
 while($ciclo=mysqli_fetch_assoc($query)){
@@ -15,6 +34,6 @@ while($ciclo=mysqli_fetch_assoc($query)){
             include("script_tabella_riparazioni.php");
     
 } 
-
+}
 mysqli_close($connessione);
 ?>
