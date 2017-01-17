028c3bc32a8f78af43d9ddccae675abeb44911a9
Stefano Foresta <forestastefano@gmail.com>
2017-01-17 11:47:23 +0100

    Modifica pagina contatti sezione fai

--- website/contatti.php
+++ website/contatti.php
@@ -107,26 +107,26 @@
 						<h3 class="contatti-titoli">Modulo di contatto</h3>
 
 						<!-- Form contatti -->
-						<form id="contatti-form" class="form-horizontal">
+						<form id="contatti-form" class="form-horizontal" method="POST" action="mailto:forestastefano@gmail.com" enctype="text/plain">
 
 							<!-- Nome -->
 							<div class="form-group">
 								<label for="inputNome" class="control-label">Nome</label>
-								<input class="form-control" type="text" id="inputNome" name="inputNome" placeholder="Nome e cognome" class="input-with-feedback">
+								<input class="form-control" type="text" id="inputNome" name="from" placeholder="Nome e cognome" class="input-with-feedback">
 							</div>
 							<!-- FINE Nome -->
 
 							<!-- Mail -->
 							<div class="form-group">
 								<label for="inputEmail" class="control-label">Email</label>
-								<input class="form-control" type="text" id="inputEmail" name="inputEmail" placeholder="mail@example.it" class="input-with-feedback">
+								<input class="form-control" type="text" id="inputEmail" name="subject" placeholder="mail@example.it" class="input-with-feedback">
 							</div>
 							<!-- FINE Mail -->
 
 							<!-- Messaggio -->
 							<div class="form-group">
 								<label for="textMessaggio" class="control-label">Messaggio</label>
-								<textarea class="form-control" id="textMessaggio" name="textMessaggio" rows="6" placeholder="Inserisci qui il tuo messaggio..." class="input-with-feedback"></textarea>
+								<textarea class="form-control" id="textMessaggio" name="body" rows="6" placeholder="Inserisci qui il tuo messaggio..." class="input-with-feedback"></textarea>
 								<br>
 								<button type="submit" class="btn btn-success btn-block">Invia</button>
 							</div>
@@ -178,7 +178,7 @@
 								<!-- Seconda domanda -->
 								<div class="panel-heading">
 									<h4 class="panel-title">
-										<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-faq" href="#faq-pannello-1">
+										<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-faq" href="#faq-pannello-2">
 											Si può restituire un prodotto acquistato?
 										</a>
 									</h4>
@@ -186,7 +186,7 @@
 								<!-- FINE Seconda domanda -->
 
 								<!-- Seconda risposta -->
-								<div id="faq-pannello-1" class="panel-collapse collapse in">
+								<div id="faq-pannello-2" class="panel-collapse collapse in">
 									<div class="panel-body">
 										<p>
 											Si, a patto che il prodotto sia stato acquistato fino a 30 giorni prima dalla data di restituzione e che sia conservato nella confezione originali senza aver subito danni. Verranno poi accreditati, sul conto corrente indicato dal cliente i soldi pari al prezzo di acquisto escluso le tasse e le spese di spedizione.
@@ -204,7 +204,7 @@
 								<!-- Terza domanda -->
 								<div class="panel-heading">
 									<h4 class="panel-title">
-										<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-faq" href="#faq-pannello-1">
+										<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-faq" href="#faq-pannello-3">
 											Quale corriere viene usato per le spedizioni?
 										</a>
 									</h4>
@@ -212,7 +212,7 @@
 								<!-- FINE Terza domanda -->
 
 								<!-- Terza risposta -->
-								<div id="faq-pannello-1" class="panel-collapse collapse in">
+								<div id="faq-pannello-3" class="panel-collapse collapse in">
 									<div class="panel-body">
 										<p>
 											I-Tech.it si appoggia a più società di distribuzione ed è il cliente a decidere con quale corriere ricevere l'oggetto acquistato. Le società a cui I-tech.it si appoggia sono le seguenti:<br>
