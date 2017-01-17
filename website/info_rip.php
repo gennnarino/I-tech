66a4c51c033d212284635343628b44fde4ef894c
Stefano Foresta <forestastefano@gmail.com>
2017-01-17 11:33:29 +0100

    Completata pagina per le info sulle riparazioni con formattazione del testo e inserimento di immagini dimostrative.

--- website/info_rip.php
+++ website/info_rip.php
@@ -1,6 +1,18 @@
-Hai rotto il tuo Smartphone o Tablet?
-Noi lo ripariamo.
-
-Riparazione in tutta Italia con ritiro e consegna a soli € 8,90
- 
-Riparazioni Garantite 6 mesi.
+<div class="container">
+	<div class="row">
+		<div class="col-sm-3">
+			<img class="img-responsive" src="assets/img/inforip/rip1.png">
+		</div>
+		<div class="col-sm-6">
+			<p class="text-info text-center">
+				<br><br><br>
+				Hai rotto il tuo Smartphone o Tablet? Noi lo ripariamo.<br>
+				Riparazione in tutta Italia con ritiro e consegna a soli € 8,90<br>
+				Riparazioni Garantite 6 mesi.
+			</p>
+		</div>
+		<div class="col-sm-3">
+			<img class="img-responsive" src="assets/img/inforip/rip2.jpg">
+		</div>
+	</div>
+</div>
