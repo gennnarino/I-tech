6dabacc1f705e0e01f912185e850b98767411134
Stefano Foresta <forestastefano@gmail.com>
2017-01-24 14:34:54 +0100

    Inserimento variabile per conservazione carrello

--- website/query_carrello.php
+++ website/query_carrello.php
@@ -1,10 +1,11 @@
 <?php 
-session_start();
-$idP=$_POST['idP'];
-$quantita=$_POST['quantita'];
-$prezzo=$_POST['prezzo'];
-$modello=$_POST['modello'];
-$carrello=$carrello.$idP.','.$modello.','.$quantita.','.$prezzo.',';
-$_SESSION['carrello']=$carrello;
-
+	session_start();
+	$carrello=$_SESSION['carrello'];
+	$idP=$_POST['idP'];
+	$quantita=$_POST['quantita'];
+	$prezzo=$_POST['prezzo'];
+	$modello=$_POST['modello'];
+	$carrello=$carrello.$idP.','.$modello.','.$quantita.','.$prezzo.',';
+	$_SESSION['carrello']=$carrello;
+	echo $carrello;
 ?>
