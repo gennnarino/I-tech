935997f5925974ab80e37b522a39f8cb6efbacf5
Gennaro Franzese <gennnarino@gmail.com>
2017-01-26 12:31:02 +0100

    no message

--- website/query_ordine.php
+++ website/query_ordine.php
@@ -2,36 +2,40 @@
  include ("connessione.php");
 session_start();
 $utente=$_SESSION['username'];
+
 $query = "SELECT cf FROM cliente WHERE email = '$utente' ";
-echo $query;
+echo $query.'<br>';
 
 $ris= mysqli_query ($connessione,$query);
 $rs = mysqli_fetch_assoc($ris);
 $cf = $rs['cf'];
-$prezzo_tot=$_POST['total_ordine'];
+$prezzo_tot=$_POST['totale_ordine'];
 $query = "INSERT INTO ordine (cf,stato,prezzo,data) VALUES ('$cf', 'PAGATO','$prezzo_tot',NOW())"; 
 $ris =mysqli_query($connessione,$query);
-echo $query;
+    echo $query.'<br>';
 
 $query = "SELECT MAX(idO) FROM ordine WHERE cf = '$cf'"; 
 $ris =mysqli_query($connessione,$query);
 $rs = mysqli_fetch_assoc($ris);
 $id=$rs['MAX(idO)'];
-echo $query;
+    echo $query.'<br>';
 $carrello=$_SESSION['carrello'];
 $prodotti=explode(',',$carrello);
 $conta = count($prodotti);
+
+
 for($i=0;$i<$conta;$i=$i+4){
 $idP=$prodotti[$i];
-$quantita=$prodotti[$i+1];
+$quantita=$prodotti[$i+2];
     
 $query ="UPDATE prodotto SET quantita=quantita-'$quantita' WHERE idP='$idP'"; 
 $ris =mysqli_query($connessione,$query);
-echo $query;
+    echo $query.'<br>';
+;
 
 $query ="INSERT INTO acquisto (idP,idO,quantita) VALUES ('$idP',$id','$quantita')";
 $ris =mysqli_query($connessione,$query);
-echo $query;
+    echo $query.'<br>';
 
 }
 
--- website/query_visualizzariparazioni.php
+++ website/query_visualizzariparazioni.php
@@ -7,16 +7,19 @@
 	     $ris= mysqli_query ($connessione,$queryy);
 	     $rs = mysqli_fetch_assoc($ris);
 	     $cf = $rs['cf'];
-            $queryyy = mysqli_query($connessione,"SELECT * FROM riparazione where cliente = '$cf' ");
-            
+         $queryyy = mysqli_query($connessione,"SELECT * FROM riparazione where cliente = '$cf' ");
+        
             while($ciclo=mysqli_fetch_assoc($queryyy)){
-          
+              if ($ciclo==0) 
+                  echo "NIENTE"; else {
                   $id = $ciclo[idR];
                   $descrizione = $ciclo[descrizione];
                   $prezzo = $ciclo[prezzo];
                   $stato = $ciclo[stato];
                   $cliente = $ciclo[cliente];
                   include("script_tabella_riparazioni.php");
+                  }
+            
             } 
     
       } 
@@ -35,4 +38,6 @@
             } 
       }
       mysqli_close($connessione);
+      
+
 ?>
