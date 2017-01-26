a2019e4e2c5bda29ed912f23bad2d7bcbcd9e42c
Stefano Foresta <forestastefano@gmail.com>
2017-01-26 12:36:49 +0100

    no message

--- website/query_ordine.php
+++ website/query_ordine.php
@@ -14,29 +14,29 @@ echo $prezzo_tot.'<br>';
 
 $query = "INSERT INTO ordine (cf,stato,prezzo,data) VALUES ('$cf', 'PAGATO','$prezzo_tot',NOW())"; 
 $ris =mysqli_query($connessione,$query);
-    echo $query.'<br>';
+echo $query.'<br>';
 
 $query = "SELECT MAX(idO) FROM ordine WHERE cf = '$cf'"; 
 $ris =mysqli_query($connessione,$query);
 $rs = mysqli_fetch_assoc($ris);
 $id=$rs['MAX(idO)'];
-    echo $query.'<br>';
+echo $query.'<br>';
 $carrello=$_SESSION['carrello'];
 $prodotti=explode(',',$carrello);
 $conta = count($prodotti);
 
 
 for($i=0;$i<$conta;$i=$i+4){
-$idP=$prodotti[$i];
-$quantita=$prodotti[$i+2];
+	$idP=$prodotti[$i];
+	$quantita=$prodotti[$i+2];
     
-$query ="UPDATE prodotto SET quantita=quantita-'$quantita' WHERE idP='$idP'"; 
-$ris =mysqli_query($connessione,$query);
+	$query ="UPDATE prodotto SET quantita=quantita-'$quantita' WHERE idP='$idP'"; 
+	$ris =mysqli_query($connessione,$query);
     echo $query.'<br>';
-;
 
-$query ="INSERT INTO acquisto (idP,idO,quantita) VALUES ('$idP',$id','$quantita')";
-$ris =mysqli_query($connessione,$query);
+
+	$query ="INSERT INTO acquisto (idP,idO,quantita) VALUES ('$idP',$id','$quantita')";
+	$ris =mysqli_query($connessione,$query);
     echo $query.'<br>';
 
 }
