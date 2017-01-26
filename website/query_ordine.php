ba00a8239d4e2740fba53a2ba8efd5e7c4b9abaa
Gennaro Franzese <gennnarino@gmail.com>
2017-01-26 11:35:40 +0100

    no message

--- website/query_ordine.php
+++ website/query_ordine.php
@@ -1,29 +1,38 @@
  <?php 
  include ("connessione.php");
-$prodotti=array();
-$prezzo_tot=$_SESSION['prezzo_tot']
-$user="a";
-$queryy = "SELECT cf FROM cliente WHERE email = '$user' ";
-$ris= mysqli_query ($connessione,$queryy);
+session_start();
+$utente=$_SESSION['username'];
+$query = "SELECT cf FROM cliente WHERE email = '$utente' ";
+echo $query;
+
+$ris= mysqli_query ($connessione,$query);
 $rs = mysqli_fetch_assoc($ris);
 $cf = $rs['cf'];
-
+$prezzo_tot=$_POST['total_ordine'];
 $query = "INSERT INTO ordine (cf,stato,prezzo,data) VALUES ('$cf', 'PAGATO','$prezzo_tot',NOW())"; 
 $ris =mysqli_query($connessione,$query);
+echo $query;
+
 $query = "SELECT MAX(idO) FROM ordine WHERE cf = '$cf'"; 
 $ris =mysqli_query($connessione,$query);
 $rs = mysqli_fetch_assoc($ris);
 $id=$rs['MAX(idO)'];
-
+echo $query;
 $carrello=$_SESSION['carrello'];
 $prodotti=explode(',',$carrello);
-
 $conta = count($prodotti);
-
 for($i=0;$i<$conta;$i=$i+4){
 $idP=$prodotti[$i];
 $quantita=$prodotti[$i+1];
+    
+$query ="UPDATE prodotto SET quantita=quantita-'$quantita' WHERE idP='$idP'"; 
+$ris =mysqli_query($connessione,$query);
+echo $query;
+
 $query ="INSERT INTO acquisto (idP,idO,quantita) VALUES ('$idP',$id','$quantita')";
 $ris =mysqli_query($connessione,$query);
+echo $query;
+
 }
 
+?>
--- website/visualizza_carrello.php
+++ website/visualizza_carrello.php
@@ -5,6 +5,13 @@ $prodotti=explode(',',$carrello);
 
 $conta = count($prodotti);
 
+if  ($conta==0){
+    
+    echo '<script language=javascript>alert("Nessun Prodotto nel carrello")</script>';
+         echo '<script language=javascript>document.location.href="prodotti.php"</script>';
+ 
+}else {
+    
 for($i=0;$i<$conta;$i=$i+4){
 $idP=$prodotti[$i];
 $modello=$prodotti[$i+1];
@@ -12,5 +19,6 @@ $quantita=$prodotti[$i+2];
 $prezzo=$prodotti[$i+3];
 include("script_tabella_carrello.php");
 $totale_ordine=$totale_ordine+$prezzo*$quantita;
+}
 }
 ?>
