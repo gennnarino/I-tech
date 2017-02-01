f21b1ebe5d62ed64bca2660b5328329164ee2cb6
Stefano Foresta <forestastefano@gmail.com>
2017-02-01 17:28:57 +0100

    no message

--- website/logout.php
+++ website/logout.php
@@ -1,7 +1,7 @@
 <?php   
-session_start(); //to ensure you are using same session
-session_destroy(); //destroy the session
-setcookie("carrello", null);
-header("location:/index.php"); //to redirect back to "index.php" after logging out
-exit();
+	session_start(); //to ensure you are using same session
+	session_destroy(); //destroy the session
+	setcookie("carrello", null);
+	header("location:/index.php"); //to redirect back to "index.php" after logging out
+	exit();
 ?>
--- website/query_visualizzariparazioni.php
+++ website/query_visualizzariparazioni.php
@@ -1,42 +1,47 @@
 <?php
       session_start(); 
       include ("connessione.php");
-      if($admin==false){
+
+      if(!$admin){
             
-         $queryy = "SELECT cf FROM cliente WHERE email = '$user' ";
-	     $ris= mysqli_query ($connessione,$queryy);
-	     $rs = mysqli_fetch_assoc($ris);
-	     $cf = $rs['cf'];
-         $queryyy = mysqli_query($connessione,"SELECT * FROM riparazione where cliente = '$cf' ");
+            $queryy = "SELECT cf FROM cliente WHERE email = '$user' ";
+            $ris= mysqli_query ($connessione,$queryy);
+            $rs = mysqli_fetch_assoc($ris);
+            $cf = $rs['cf'];
+            $queryyy = mysqli_query($connessione,"SELECT * FROM riparazione where cliente = '$cf' ");
         
             while($ciclo=mysqli_fetch_assoc($queryyy)){
-              if ($ciclo==0) 
-                  echo "NIENTE"; else {
+               if ($ciclo==0){
+
+               } 
+               else {
                   $id = $ciclo[idR];
                   $descrizione = $ciclo[descrizione];
                   $prezzo = $ciclo[prezzo];
                   $stato = $ciclo[stato];
                   $cliente = $ciclo[cliente];
                   include("script_tabella_riparazioni.php");
-                  }
+               }
             
             } 
     
       } 
-      else if($admin==true){
-    
+      else if($admin){
+            echo $_SESSION['admin'].'<br>';
             $query = mysqli_query($connessione,"SELECT * FROM riparazione");
-
+            echo $_SESSION['admin'].'<br>';
             while($ciclo=mysqli_fetch_assoc($query)){
-          
+                  echo $_SESSION['admin'].'<br>';
                   $id = $ciclo[idR];
                   $descrizione = $ciclo[descrizione];
                   $prezzo = $ciclo[prezzo];
                   $stato = $ciclo[stato];
                   $cliente = $ciclo[cliente];
                   include("script_tabella_riparazioni.php");
-            } 
+            }
+            echo $_SESSION['admin'].'<br>'; 
       }
+      echo $_SESSION['admin'].'<br>';
       mysqli_close($connessione);
       
 
